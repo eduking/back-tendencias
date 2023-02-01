c8e9f1d1fca78577bc05f24873f5ab9253c6d732
eduking <jez.nevarez@yavirac.edu.ec>
2023-02-01 00:13:53 -0500

    cambios

---
+++ src/modules/ventas/entities/category.entity.ts
@@ -0,0 +1,118 @@
+import { create } from "domain";
+import { title } from "process";
+import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn ,
+    DeleteDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+
+} from "typeorm";
+
+@Entity('category',{schema: 'ventas'} 
+export class CategoryEntity{
+    @PrimaryGeneratedColumn(
+        @PrimaryGeneratedColumn(sv)
+    )
+    id:number;
+}
+
+@CreateDateColumn({
+    name:'created_date',
+    type: 'timestamptz' ,
+    default:() =>'CURRENT_TIMESTAP'
+
+})
+createdAt:Date;
+
+@UpdateDateColumn({
+    name:'updated_date',
+    type: 'timestamptz' ,
+    default:() =>'CURRENT_TIMESTAP'
+
+})
+updatedAt: Date;
+
+
+@DeleteDateColumn({
+
+    name:'deleted_date',
+    type: 'timestamptz' ,
+    nullable :true,
+})
+deletedAt:Date;
+
+
+
+
+//RelationShip
+@ManyToOne(() => CategoryEntity, (category)=> category.product)
+category: CategoryEntity;
+
+
+
+@Column('varchar',{
+name:title,
+unique:true,
+comment: 'titulo del producto'
+})
+title:string;
+
+@Column ('integer',{
+name:'price',
+comment: 'precio del producto condos decimales ',
+})
+price :number;
+
+@Column('varchar',{
+name:'description',
+nullable :true,
+comment:'descripcion del producto'
+})
+description:string;
+
+/*@Column('boolean',{
+name:'estate',
+
+})
+state:boolean;*/
+/*@Column('enum',{
+name:'language',
+enum:languageTypeEnum,
+
+})
+language:LanguageTypeEnum;*/
+
+
+//Completar 
+@BeforeInsert()
+@BeforeUpdate()
+
+async setTitle(){
+if(!this.title){
+return;
+}
+this.title = this.title.toUpperCase();
+}
+/*setEmail(){
+return;
+}
+this.email = this.email.toLowerCase().trim();
+
+async hashPassword(){
+
+if(!this.password){
+return;
+}
+this.password = await Bcrypt.hash();
+}*/
+
+}
+
+
+
+
+
+
+
+
